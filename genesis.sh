9b5ee7d3cfac93a0ab3cbfef36069250b3021039
Chris Drumgoole <chris.drumgoole@vuulr.com>
2018-02-27 16:17:25 +0800

    fixed issue with non-hex char in genesis removed setting on RPC_PORT

--- genesis.sh
+++ genesis.sh
@@ -7,11 +7,11 @@
 #
 # Runs generates a unique genesis block for our purposes.
 #
-# GEN_NONCE: Vuulr private chain dev set to "0xaabaa78274dfdeas"
+# GEN_NONCE: Vuulr private chain dev set to "0xaabaa78274dfdeaa"
 # GEN_CHAIN_ID: Vuulr private chain dev set to 47271
 #* GEN_ALLOC: Pre-allocated balance to 100000000 for Vuulr
 
-GEN_NONCE="0xaabaa78274dfdeas"
+GEN_NONCE="0xaabaa78274dfdeaa"
 GEN_CHAIN_ID=47271
 GEN_ALLOC='"0x0000000000000000000000000000000000000000": {"balance": "100000000"}'
 sed "s/\${GEN_NONCE}/$GEN_NONCE/g" src/genesis.json.template | sed "s/\${GEN_ALLOC}/$GEN_ALLOC/g" | sed "s/\${GEN_CHAIN_ID}/$GEN_CHAIN_ID/g" > genesis.json
